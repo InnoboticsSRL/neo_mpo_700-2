c0e216eab3d20ab89803fcc3ecf6f03132793489
mdei <mattydr98@gmail.com>
2025-06-10 14:02:34 +0200

    fixed bug with systemd service

--- configs/teleop/teleop.launch.py
+++ configs/teleop/teleop.launch.py
@@ -19,17 +19,10 @@ def generate_launch_description():
             parameters = [config])
     ,
      launch_ros.actions.Node(
-            package='joy', 
-            executable='joy_node', 
+            package='joy_linux', 
+            executable='joy_linux_node', 
             output='screen',
             name='joy_node',
             namespace=robot_namespace,
             parameters = [{'dev': "/dev/input/js0"}, {'deadzone':0.12}]),
-    # launch_ros.actions.Node(
-    #         package='joy_linux', 
-    #         executable='joy_linux_node', 
-    #         output='screen',
-    #         name='joy_node',
-    #         namespace=robot_namespace,
-    #         parameters = [{'dev': "/dev/input/js0"}, {'deadzone':0.12}])
     ])
